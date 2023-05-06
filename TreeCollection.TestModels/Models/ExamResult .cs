d216557235c864b5aa4edb71f0a31a8cad304a3b
Nahuel Rodriguez <nahuelrmaury@gmail.com>
2023-05-06 00:30:25 -0300

    first part of the task

--- TreeCollection.TestModels/Models/ExamResult .cs
+++ TreeCollection.TestModels/Models/ExamResult .cs
@@ -18,5 +18,27 @@ namespace TreeCollection.TestModels.Models
             Score = score;
             Date = date;
         }
+
+        public int CompareTo(ExamResult other)
+        {
+            /* variables to compare */
+            int nameValue = this.Name.CompareTo(other.Name);
+            int dateValue = this.Date.CompareTo(other.Date);
+            int idValue = this.Id.CompareTo(other.Id);
+
+            /* if names are equal, ask for date */
+            if (nameValue == 0)
+            {
+                /* then, if dates values are equal, return id */
+                if (dateValue == 0)
+                {
+                    return idValue;
+                }
+                /* if names are equal but dates are not, then return date value*/
+                return dateValue;
+            }
+            /* if names are not equal, then return name value */
+            return nameValue;
+        }
     }
 }
